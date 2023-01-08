373ef74cfa0b033a35244db8a254dffb3f2a7bef
Farmer Diamonds <113501703+farmerdiamonds@users.noreply.github.com>
2023-01-09 00:48:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ async function main(){
   // Genesiscoin to start search
   process.env.genesisCoin = '0x9456a7d207802336ebe872d80e00a94d0ef69027500f81a9039083611da472e3'; //PLOT Genesiscoin
   let CATCoins = await getCATCoins();
-  console.log("Combinig coins in at same targetaddress ...");
   console.log("Found "+CATCoins.length+" unspent Coins");
+  console.log("Combinig coins with same targetaddress ...");
   CATCoins = combineTargets(CATCoins);
-  console.log("Found "+CATCoins.length+" different addresse");
+  console.log("Found "+CATCoins.length+" different addresses");
   const CATAmount = sumCATCoins(CATCoins);
   console.log("Found "+CATAmount+" CAT mojos on chain");
 }
